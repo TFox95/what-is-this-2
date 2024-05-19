09c0452f0894520fb56fca2ec73559ce5f4291f0
Thomas Fox <TFox95@aol.com>
2024-05-19 15:26:47 -0500

    test

--- simple/api/foo.rs
+++ simple/api/foo.rs
@@ -1,6 +1,7 @@
 // Example api/foo.rs
-use vercel_runtime::{Body, Error, Request, Response, StatusCode};
+use vercel_runtime::{Body, Error, Request, Response, StatusCode, bundled_api};
 
+#[bundled_api]
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
     Ok(Response::builder()
         .status(StatusCode::OK)
